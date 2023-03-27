4ae8335c3a99fda04e864e4eee4c2c455c383f60
Changseo Jang <changseo.jang@riiid.co>
2023-03-27 15:52:59 +0900

    better test dependency handling

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,11 +1,22 @@
-local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
-local plenary_not_exists = vim.fn.isdirectory(plenary_dir) == 0
-if plenary_not_exists then
-  vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
+local ensure_install = function(plugin)
+	local base_dir = os.getenv("BASE_DIR") or "/tmp/"
+	local plugin_name = vim.split(plugin, "/")[2]
+
+	local plugin_dir = base_dir .. plugin_name
+
+	local plugin_not_exists = vim.fn.isdirectory(plugin_dir) == 0
+	if plugin_not_exists then
+		print("Installing " .. plugin_name)
+		vim.fn.system({ "git", "clone", "https://github.com/" .. plugin, plugin_dir })
+	end
+
+	vim.opt.runtimepath:append(plugin_dir)
 end
 
+ensure_install("nvim-lua/plenary.nvim")
+ensure_install("nvim-treesitter/nvim-treesitter")
+
 vim.opt.runtimepath:append(".")
-vim.opt.runtimepath:append(plenary_dir)
 
 vim.cmd("runtime plugin/plenary.vim")
 require("plenary.busted")
