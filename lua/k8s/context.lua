96ad9024b663ffda16dc2279c32f994aeff4092d
BeLeap <beleap@beleap.dev>
2023-03-28 23:39:34 +0900

    add get context feature

--- lua/k8s/context.lua
+++ lua/k8s/context.lua
@@ -18,9 +18,11 @@ local context_names_query = [[
                      (block_node
                        (block_mapping
                          (block_mapping_pair
-                           key: ((flow_node) @field
-                                 (#eq? @field "name"))
-                           value: (flow_node) @capture
+                           key:   ((flow_node) @field
+                                   (#eq? @field "name"))
+                           value: (flow_node
+                                    (plain_scalar) @capture
+                                  )
                          )
                        )
                      )
@@ -33,20 +35,21 @@ local context_names_query = [[
 )
 ]]
 
-M.read_config = function()
+M.get = function()
 	local content = utils.readfile(constants.kubeconfig)
 
 	local parser = vim.treesitter.get_string_parser(content, "yaml")
 	local tree = parser:parse()
 
-	-- for key, value in pairs(getmetatable(tree[1])) do
-	-- 	print(key, value)
-	-- end
-
 	local query = vim.treesitter.query.parse("yaml", context_names_query)
-	for id, node, metadata in query:iter_captures(tree[1]:root(), parser:source(), 0, 1000) do
-		print(vim.treesitter.get_node_text(node, parser:source()))
+	local contexts = {}
+	for _, node, _ in query:iter_captures(tree[1]:root(), parser:source(), 0, 1000) do
+		if node:type() == "plain_scalar" then
+			vim.list_extend(contexts, { vim.treesitter.get_node_text(node, parser:source()) })
+		end
 	end
+
+	return contexts
 end
 
 return M
--- tests/k8s/context_spec.lua
+++ tests/k8s/context_spec.lua
@@ -1,7 +1,51 @@
 local context = require("k8s.context")
+local utils = require("k8s.utils")
+local mock = require("luassert.mock")
 
-describe("Test", function()
-	it("1", function()
-		context.read_config()
+describe("contexts", function()
+	it("get", function()
+		it("should return list of contexts", function()
+			local mock_utils = mock(utils, true)
+
+			local mock_kubeconfig = [[
+      apiVersion: v1
+      kind: Config
+      clusters:
+      - name: minikube-1
+        cluster:
+          certificate-authority: /home/foo/.minikube/ca.crt
+          server: https://172.20.0.1:8443
+      - name: minikube-2
+        cluster:
+          certificate-authority: /home/bar/.minikube/ca.crt
+          server: https://172.20.0.2:8443
+      users:
+      - name: minikube-1
+        user:
+          client-certificate: /home/foo/.minikube/profiles/minikube/client.crt
+          client-key: /home/foo/.minikube/profiles/minikube/client.key
+      - name: minikube-2
+        user:
+          client-certificate: /home/bar/.minikube/profiles/minikube/client.crt
+          client-key: /home/bar/.minikube/profiles/minikube/client.key
+      contexts:
+      - name: minikube-1
+        context:
+          cluster: minikube-1
+          namespace: default
+          user: minikube-1
+      - name: minikube-2
+        context:
+          cluster: minikube-2
+          namespace: default
+          user: minikube-2
+      current-context: minikube-1
+      ]]
+
+			mock_utils.readfile.returns(mock_kubeconfig)
+
+			local contexts = context.get()
+			assert.are.same({ "minikube-1", "minikube-2" }, contexts)
+		end)
 	end)
 end)
