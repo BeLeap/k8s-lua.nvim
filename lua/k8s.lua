db2445ef356ca9fff595ee96f998e28721f427ec
Changseo Jang <changseo.jang@riiid.co>
2023-03-29 12:22:33 +0900

    format

--- lua/k8s.lua
+++ lua/k8s.lua
@@ -1,15 +1,15 @@
 local M = {}
 
 M.config = {
-  kubeconfig_location = os.getenv("HOME") .. "/.kube/config"
+	kubeconfig_location = os.getenv("HOME") .. "/.kube/config",
 }
 
 M.setup = function(args)
-  if args.kubeconfig_location ~= nil then
-    args.kubeconfig_location = vim.fs.normalize(args.kubeconfig_location)
-  end
+	if args.kubeconfig_location ~= nil then
+		args.kubeconfig_location = vim.fs.normalize(args.kubeconfig_location)
+	end
 
-  M.config = vim.tbl_deep_extend("force", M.config, args or {})
+	M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
 return M
