d746af67aeca28245ca374e3c74eae1fcd9f06b6
Changseo Jang <changseo.jang@riiid.co>
2023-03-29 12:17:50 +0900

    normalize kubeconfig path

--- lua/k8s.lua
+++ lua/k8s.lua
@@ -1,10 +1,14 @@
 local M = {}
 
 M.config = {
-  kubeconfig_location = "~/.kube/config"
+  kubeconfig_location = os.getenv("HOME") .. "/.kube/config"
 }
 
 M.setup = function(args)
+  if args.kubeconfig_location ~= nil then
+    args.kubeconfig_location = vim.fs.normalize(args.kubeconfig_location)
+  end
+
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
