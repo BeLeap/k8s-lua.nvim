2b044e2942d7d5fc01fa7a664fccbaaca9be26d0
Changseo Jang <changseo.jang@riiid.co>
2023-03-29 18:32:56 +0900

    feat/context/get current (#1)
    * test: add failing spec
    
    * feature: load current context
    
    * test: add spec for empty current-context

--- tests/k8s/context_spec.lua
+++ tests/k8s/context_spec.lua
@@ -45,6 +45,58 @@ describe("contexts", function()
 		}
 	end)
 
+	it("_get_current", function()
+		it("should return current context", function()
+			local mock_utils = mock(utils, true)
+			mock_utils.readfile.returns(mock_kubeconfig)
+
+			local contexts = context._get_current()
+			assert.are.same("minikube-1", contexts)
+		end)
+
+		it("should return nil if no current-context specified", function()
+			local mock_kubeconfig_without_current_context = [[
+      apiVersion: v1
+      kind: Config
+      clusters:
+      - name: minikube-1
+        cluster:
+          certificate-authority: /home/foo/.minikube/ca.crt
+          server: https://172.20.0.1:8443
+      - name: minikube-2
+        cluster:
+          certificate-authority: /home/bar/.minikube/ca.crt
+          server: https://172.20.0.2:8443
+      users:
+      - name: minikube-1
+        user:
+          client-certificate: /home/foo/.minikube/profiles/minikube/client.crt
+          client-key: /home/foo/.minikube/profiles/minikube/client.key
+      - name: minikube-2
+        user:
+          client-certificate: /home/bar/.minikube/profiles/minikube/client.crt
+          client-key: /home/bar/.minikube/profiles/minikube/client.key
+      contexts:
+      - name: minikube-1
+        context:
+          cluster: minikube-1
+          namespace: default
+          user: minikube-1
+      - name: minikube-2
+        context:
+          cluster: minikube-2
+          namespace: default
+          user: minikube-2
+      ]]
+
+			local mock_utils = mock(utils, true)
+			mock_utils.readfile.returns(mock_kubeconfig_without_current_context)
+
+			local contexts = context._get_current()
+			assert.are.same(nil, contexts)
+		end)
+	end)
+
 	it("_get_list", function()
 		it("should return list of contexts", function()
 			local mock_utils = mock(utils, true)
