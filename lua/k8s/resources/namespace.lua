4897e47b1a79ce166316b31b37d397e9cb84ea60
Changseo Jang <changseo.jang@riiid.co>
2023-03-31 12:26:40 +0900

    feat: get list of namespaces

--- lua/k8s/resources/namespace.lua
+++ lua/k8s/resources/namespace.lua
@@ -1,4 +1,5 @@
 local curl = require("plenary.curl")
+local iterators = require("plenary.iterators")
 
 local proxy = require("k8s.api.proxy")
 
@@ -18,7 +19,13 @@ M.list = function()
 
     if res ~= nil then
         local data = vim.fn.json_decode(res.body)
-        print(vim.inspect(data))
+
+        local items_iter = iterators.iter(data.items)
+        local names_iter = items_iter:map(function(item)
+            return item.metadata.name
+        end)
+
+        vim.ui.select(names_iter:tolist(), {}, function(_choice) end)
     end
 end
 
