db90f535251d16244d70112ef026f671449cfca9
BeLeap <beleap@beleap.dev>
2023-04-13 18:46:24 +0900

    wip

--- lua/k8s/utils.lua
+++ lua/k8s/utils.lua
@@ -14,4 +14,45 @@ M.join_to_string = function(data)
     return table.concat(content, "\n")
 end
 
+M.calculate_diffs = function(original, new)
+    if type(original) ~= "table" or type(new) ~= "table" then
+        local diff = {
+            op = "",
+            path = "",
+            value = new,
+        }
+        if original ~= nil and new ~= nil then
+            diff.op = "replace"
+        elseif original ~= nil and new == nil then
+            diff.op = "remove"
+        elseif original == nil and new ~= nil then
+            diff.op = "add"
+        end
+
+        return { diff }
+    end
+
+    local original_keys = vim.tbl_keys(original)
+    local new_keys = vim.tbl_keys(new)
+    local keys = vim.tbl_extend("keep", original_keys, new_keys)
+
+    print(vim.inspect(original_keys))
+    print(vim.inspect(new_keys))
+    print(vim.inspect(keys))
+
+    local diffs = {}
+    for _, key in ipairs(keys) do
+        if not vim.deep_equal(original[key], new[key]) then
+            local sub_diffs = M.calculate_diffs(original[key], new[key])
+
+            for _, sub_diff in ipairs(sub_diffs) do
+                sub_diff.path = "/" .. key .. sub_diff.path
+
+                table.insert(diffs, sub_diff)
+            end
+        end
+    end
+
+    return diffs
+end
 return M
