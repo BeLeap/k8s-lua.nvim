37fcfe2189c468ba9d1311e73deab66a9ad89118
BeLeap <beleap@beleap.dev>
2023-04-14 17:27:00 +0900

    test: add union test

--- tests/k8s/util_spec.lua
+++ tests/k8s/util_spec.lua
@@ -13,4 +13,33 @@ describe("utils", function()
             assert.are.equal("foo\nbar", result)
         end)
     end)
+
+    it("union", function()
+        it("should concat list without duplicated item", function()
+            local list1 = { 1, 2, 3, 4, 5 }
+            local list2 = { 1, 3, 5, 7, 9 }
+
+            local result = utils.union(list1, list2)
+
+            local hash = {}
+            for _, item in ipairs(result) do
+                if hash[item] then
+                    assert(false, "duplicated item exists")
+                else
+                    hash[item] = true
+                end
+            end
+
+            local expected_hash = {
+                [1] = true,
+                [2] = true,
+                [3] = true,
+                [4] = true,
+                [5] = true,
+                [7] = true,
+                [9] = true,
+            }
+            assert.are.same(expected_hash, hash)
+        end)
+    end)
 end)
