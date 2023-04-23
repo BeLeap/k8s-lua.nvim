1a4c89dbda075020e03f489edfb34bd81f4510aa
BeLeap <beleap@beleap.dev>
2023-04-23 22:15:39 +0900

    feat: simple listing buffer

--- lua/k8s/ui/namespace.lua
+++ lua/k8s/ui/namespace.lua
@@ -1,25 +1,14 @@
 local pickers = require("k8s.ui.pickers")
-local global_contexts = require("k8s.global_contexts")
 local resources = require("k8s.resources")
 
 local M = {}
 
 M.select = function()
     local namespace = resources:new("namespaces", "api/v1", false, nil)
-    local Picker = pickers:new(namespace, {
-        when_select = function(selection)
-            global_contexts.selected_namepace = selection.value.metadata.name
-        end,
-        is_current = function(entry)
-            if entry.metadata.name == global_contexts.selected_namepace then
-                return true
-            end
+    local picker = pickers:new(namespace, {})
 
-            return false
-        end,
-    })
-
-    Picker.picker:find()
+    vim.api.nvim_buf_attach(picker.buffer, false, {})
+    vim.api.nvim_set_current_buf(picker.buffer)
 end
 
 return M
--- lua/k8s/ui/pickers/init.lua
+++ lua/k8s/ui/pickers/init.lua
@@ -1,13 +1,3 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local previewers = require("telescope.previewers")
-local conf = require("telescope.config").values
-local action_state = require("telescope.actions.state")
-local actions = require("telescope.actions")
-
-local utils = require("k8s.utils")
-local detail = require("k8s.ui.pickers.detail")
-
 ---@class ResourceEntry
 ---@field value KubernetesObject
 ---@field display string
@@ -15,100 +5,29 @@ local detail = require("k8s.ui.pickers.detail")
 
 ---@class ResourcesPicker
 ---@field private resources Resource
----@field private result KubernetesObject[]
----@field public picker Picker
+---@field public buffer BufferHandle
 local ResourcesPicker = {}
 
-function ResourcesPicker:edit_action(prompt_bufnr)
-    return function()
-        ---@type ResourceEntry
-        local selection = action_state.get_selected_entry()
-
-        local buffer = detail.create(self.resources.kind, selection.value, function(ev)
-            if self.resources.patch ~= nil then
-                local content_raw = utils.join_to_string(vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false))
-                local content = load("return " .. content_raw)()
-                local diff = utils.calculate_diffs(selection.value, content)
-
-                self.resources:patch(selection.value.metadata, vim.json.encode(diff))
-            end
-        end)
-
-        actions.close(prompt_bufnr)
-        vim.api.nvim_set_current_buf(buffer)
-    end
-end
-
-function ResourcesPicker:preview_opts_factory()
-    return {
-        title = "detail",
-        dyn_title = function(_, entry)
-            return "detail - " .. entry.display
-        end,
-        define_preview = function(preview, entry, _status)
-            vim.api.nvim_buf_set_option(preview.state.bufnr, "ft", "lua")
-            vim.api.nvim_buf_set_lines(
-                preview.state.bufnr,
-                0,
-                -1,
-                false,
-                vim.fn.split(tostring(vim.inspect(entry.value)), "\n")
-            )
-        end,
-    }
-end
-
 ---@param resources Resource
----@param args { when_select: function|nil, is_current: function|nil }
+---@param args {}
 function ResourcesPicker:new(resources, args)
     self.resources = resources
-    ---@type KubernetesObject[]
-    self.results = self.resources:list_iter():tolist()
 
-    local when_select = args.when_select or function(_selection) end
-    local is_current = args.is_current or function(_elem)
-        return false
+    local iter = self.resources:list_iter()
+
+    if iter ~= nil then
+        local names = iter:map(
+            ---@param elem KubernetesObject
+            function(elem)
+                return elem.metadata.name
+            end
+        ):tolist()
+
+        self.buffer = vim.api.nvim_create_buf(true, true)
+        vim.api.nvim_buf_set_name(self.buffer, "k8s://" .. self.resources.kind)
+        vim.api.nvim_buf_set_option(self.buffer, "buftype", "")
+        vim.api.nvim_buf_set_lines(self.buffer, 0, -1, false, names)
     end
-    local default_selection_index = 0
-    for i, elem in ipairs(self.results) do
-        if is_current(elem) then
-            default_selection_index = i
-        end
-    end
-
-    self.picker = pickers.new({}, {
-        prompt_title = self.resources.kind,
-        finder = finders.new_table({
-            results = self.results,
-            ---@param entry KubernetesObject
-            entry_maker = function(entry)
-                ---@type ResourceEntry
-                local resource_entry = {
-                    value = entry,
-                    display = entry.metadata.name,
-                    ordinal = entry.metadata.name,
-                }
-
-                return resource_entry
-            end,
-        }),
-        default_selection_index = default_selection_index,
-        sorter = conf.generic_sorter(),
-        attach_mappings = function(prompt_bufnr, map)
-            map("n", "e", self:edit_action(prompt_bufnr))
-
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-
-                ---@type ResourceEntry
-                local selection = action_state.get_selected_entry()
-                when_select(selection)
-            end)
-
-            return true
-        end,
-        previewer = previewers.new_buffer_previewer(self:preview_opts_factory()),
-    })
 
     return self
 end
